7a0a8c5f961f7a0b8900d8fb448505b1c5bdccb1
sharif-github <107545661+sharif-github@users.noreply.github.com>
2024-02-23 00:07:38 +0600

    production url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://car-doctor-server-gamma-lilac.vercel.app'],
   credentials: true
 }))
 app.use(express.json());
